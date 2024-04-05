c90e16f700959732509f615431a68c220778751d
doxuanhop <47825416+doxuanhop@users.noreply.github.com>
2024-04-05 12:54:04 +0700

    Update install.sh

--- scripts/install/install.sh
+++ scripts/install/install.sh
@@ -3,73 +3,58 @@
 # Exit immediately if any command exits with non-zero exit status.
 set -e
 
+# Kiểm tra xem Termux đã cài đặt `unzip` chưa
 if ! command -v unzip >/dev/null; then
 	echo 'err: `unzip` is required to install Rush. Please install it and try again.'
 	exit 1
 fi
 
-if [[ -v RUSH_HOME]]; then
+# Thiết lập biến môi trường RUSH_HOME
+if [[ -v RUSH_HOME ]]; then
   rushHome="$RUSH_HOME"
 else
-  if ! -v rush >/dev/null; then
-    rushHome="$HOME/.rush"
-  else
-    rushHome="$(dirname $(dirname $(which rush)))"
-  fi
+  rushHome="$HOME/.rush"
 fi
 
-if [ "$OS" = "Windows_NT" ]; then
-  target="x86_64-windows"
-else
-  case $(uname -sm) in
-  "Darwin x86_64") target="x86_64-apple-darwin" ;;
-  "Darwin arm64") target="arm64-apple-darwin" ;;
-  *) target="x86_64-linux" ;;
-  esac
-fi
+# Xác định target dựa trên hệ điều hành
+case $(uname -sm) in
+"Darwin x86_64") target="x86_64-apple-darwin" ;;
+"Darwin arm64") target="arm64-apple-darwin" ;;
+*) target="x86_64-linux" ;;
+esac
 
+# Xác định URL của file zip dựa trên target
 zipUrl="https://github.com/shreyashsaitwal/rush-cli/releases/latest/download/rush-$target.zip"
-curl --location --progress-bar -o "$rushHome/rush-$target.zip" "$zipUrl"
 
+# Tải và giải nén file zip
+curl --location --progress-bar -o "$rushHome/rush-$target.zip" "$zipUrl"
 unzip -oq "$rushHome/rush-$target.zip" -d "$rushHome"/
 rm "$rushHome/rush-$target.zip"
 
-# Make the Rush binary executable on Unix systems. 
-if [ ! "$OS" = "Windows_NT" ]; then
-  chmod +x "$rushHome/bin/rush"
-fi
+# Đặt quyền thực thi cho tệp binary của Rush
+chmod +x "$rushHome/bin/rush"
 
+# Hiển thị thông báo cài đặt thành công
 echo
 echo "Successfully downloaded the Rush CLI binary at $rushHome/bin/rush"
 
-# Prompt user of they want to download dev dependencies now.
+# Hỏi người dùng có muốn tải các thư viện Java cần thiết không
 echo "Now, proceeding to download necessary Java libraries (approx size: 170 MB)."
 read -p "Do you want to continue? (Y/n) " -n 1 -r
 echo
 
+# Tải các thư viện Java nếu người dùng chọn tiếp tục
 if [[ ! $REPLY =~ ^[Nn]$ ]]; then
-  if [ "$OS" = "Windows_NT" ]; then
-    "./$rushHome/bin/rush.exe" deps sync --dev-deps --no-logo
-  else
-    "./$rushHome/bin/rush" deps sync --dev-deps --no-logo
-  fi
+  "$rushHome/bin/rush" deps sync --dev-deps --no-logo
 fi
 
+# Hiển thị thông báo cài đặt thành công
 echo
-if [[ ! $REPLY =~ ^[Nn]$ ]]; then
-  echo "Success! Installed Rush at $rushHome/bin/rush"
-else
-  echo "Rush has been partially installed at $rushHome/bin/rush"
-  echo 'Please run `rush deps sync --dev-deps` to download the necessary Java libraries.'
-fi
+echo "Success! Installed Rush at $rushHome/bin/rush"
 
-case $SHELL in
-  /bin/zsh) shell_profile=".zshrc" ;;
-  *) shell_profile=".bash_profile" ;;
-esac
-
+# Thêm đoạn mã vào profile của Termux
 echo
-echo "Now, add the following to your \$HOME/$shell_profile (or similar):"
+echo "Now, add the following to your ~/.bashrc (or similar):"
 echo "export PATH=\"\$PATH:$rushHome/bin\""
 
 echo
